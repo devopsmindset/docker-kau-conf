56c615cee7f645e2a56f37c356541cb03b76297e
canoberg <guillem.cano_bergada@roche.com>
2022-12-22 15:42:40 +0100

    commented out part that caused issue

--- system-configuration.sh
+++ system-configuration.sh
@@ -24,31 +24,28 @@ sudo apt-get -y install software-properties-common
 sudo apt-get -y install ansible
 
 # Verify packages are installed
-#packages=("zip" "ansible" "ca-certificates")
-#for pkg in ${packages[@]}; do
-#
-#	is_pkg_installed=$(dpkg-query -W --showformat='${Status}\n' ${pkg} | grep "install ok installed")
-#	if [ "${is_pkg_installed}" == "install ok installed" ]; then
-#		echo ${pkg} is installed.
-#	else
-#		echo ${pkg} has not been correctly installed.
-#		exit 3
-#	fi
-#done
+packages=("zip" "ansible" "ca-certificates")
+for pkg in ${packages[@]}; do
+
+	is_pkg_installed=$(dpkg-query -W --showformat='${Status}\n' ${pkg} | grep "install ok installed")
+	if [ "${is_pkg_installed}" == "install ok installed" ]; then
+		echo ${pkg} is installed.
+	else
+		echo ${pkg} has not been correctly installed.
+		exit 3
+	fi
+done
 
 # Install Roche certificates
-sudo apt-get -y update
-echo "apt-get update done"
-cd /usr/local/share/ca-certificates
-echo "cd done"
-sudo wget --no-check-certificate --no-verbose http://repository.kiosk.roche.com/public/certificates/roche.com/roche_com_enterprise.crt -O roche_com_enterprise.crt
-eho "wget 1 done"
-sudo wget --no-check-certificate --no-verbose http://repository.kiosk.roche.com/public/certificates/roche.com/roche_com_root.crt -O roche_com_root.crt
-sudo wget --no-check-certificate --no-verbose http://repository.kiosk.roche.com/public/certificates/roche.com/geo_trust.crt -O geo_trust.crt
-sudo wget --no-check-certificate --no-verbose http://certinfo.roche.com/rootcerts/Roche%20G3%20Root%20CA.crt -O roche_com_CA1_G3.crt
-sudo wget --no-check-certificate --no-verbose http://certinfo.roche.com/rootcerts/Roche%20Root%20CA%201%20-%20G2.crt -O roche_com_CA1_G2.crt
-sudo wget --no-check-certificate --no-verbose http://certinfo.roche.com/rootcerts/Roche%20Root%20CA%201.crt -O roche_com_CA1.crt
-update-ca-certificates
+#sudo apt-get -y update
+#cd /usr/local/share/ca-certificates
+#sudo wget --no-check-certificate --no-verbose http://repository.kiosk.roche.com/public/certificates/roche.com/roche_com_enterprise.crt -O roche_com_enterprise.crt
+#sudo wget --no-check-certificate --no-verbose http://repository.kiosk.roche.com/public/certificates/roche.com/roche_com_root.crt -O roche_com_root.crt
+#sudo wget --no-check-certificate --no-verbose http://repository.kiosk.roche.com/public/certificates/roche.com/geo_trust.crt -O geo_trust.crt
+#sudo wget --no-check-certificate --no-verbose http://certinfo.roche.com/rootcerts/Roche%20G3%20Root%20CA.crt -O roche_com_CA1_G3.crt
+#sudo wget --no-check-certificate --no-verbose http://certinfo.roche.com/rootcerts/Roche%20Root%20CA%201%20-%20G2.crt -O roche_com_CA1_G2.crt
+#sudo wget --no-check-certificate --no-verbose http://certinfo.roche.com/rootcerts/Roche%20Root%20CA%201.crt -O roche_com_CA1.crt
+#update-ca-certificates
 
 # Python installation
 sudo apt-get -y update
@@ -58,20 +55,18 @@ sudo apt-get -y install python-pip
 sudo apt-get -y install python3-pip
 
 # Verify packages are installed
-#packages=("git" "gcc" "python-pip" "python3-pip")
-#for pkg in ${packages[@]}; do
-#
-#	is_pkg_installed=$(dpkg-query -W --showformat='${Status}\n' ${pkg} | grep "install ok installed")
-#	if [ "${is_pkg_installed}" == "install ok installed" ]; then
-#		echo ${pkg} is installed.
-#		if [ "${pkg}" == "python-pip" ]; then
-#			sudo pip install pywinrm
-#		else
-#			sudo pip3 install pywinrm
-#		fi
-#	fi
-#done
-sudo pip install pywinrm
-sudo pip3 install pywinrm
+packages=("git" "gcc" "python-pip" "python3-pip")
+for pkg in ${packages[@]}; do
+
+	is_pkg_installed=$(dpkg-query -W --showformat='${Status}\n' ${pkg} | grep "install ok installed")
+	if [ "${is_pkg_installed}" == "install ok installed" ]; then
+		echo ${pkg} is installed.
+		if [ "${pkg}" == "python-pip" ]; then
+			sudo pip install pywinrm
+		else
+			sudo pip3 install pywinrm
+		fi
+	fi
+done
 exit 0
 
